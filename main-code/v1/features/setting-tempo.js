9fe2043177058c90b91940954e162f903261672e
Maciej Sawicki <saw.maciej@gmail.com>
2017-04-07 21:27:29 +0200

    commit

--- main-code/v1/features/setting-tempo.js
+++ main-code/v1/features/setting-tempo.js
@@ -1,1 +1,20 @@
 ReactiveLocalStorage.setDefaultParam('tempo', '128');
+
+var colorsOrder = [
+	'red',
+	'white',
+	'black'
+];
+
+function startChangingColorsWithTempo(colorsOrder, tempo) {
+
+	var colorIndex = 0; 
+	var tempo = 60000/ReactiveLocalStorage.getParam('tempo');
+
+	setInterval(function() {
+		var colorToSet = colorsOrder[colorIndex];
+		$('.main-visualisations__fullscreen-color').css('background-color', colorToSet);
+		colorIndex = colorIndex + 1;
+	}, tempo)
+
+}

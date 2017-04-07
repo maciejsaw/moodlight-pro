c8739794ec351d3d5f4f2f5373c6f67701c611d0
Maciej Sawicki <saw.maciej@gmail.com>
2017-04-07 22:37:03 +0200

    commit

--- main-code/v1/get-scripts.js
+++ main-code/v1/get-scripts.js
@@ -22,6 +22,8 @@ var scriptsToLoad = [
 	"https://maciejsaw.github.io/moodlight-pro/general-framework/initial-preloading.js", //preloading needs to be after other framework files
 	"https://maciejsaw.github.io/moodlight-pro/main-code/v1/features/setting-tempo.js",
 	"https://maciejsaw.github.io/moodlight-pro/main-code/v1/features/starting-and-stopping.js",
+	"https://maciejsaw.github.io/moodlight-pro/main-code/v1/features/auto-hiding-menu.js",
+
 ]; 
 
 https://maciejsaw.github.io/moodlight-pro/main-code/v1

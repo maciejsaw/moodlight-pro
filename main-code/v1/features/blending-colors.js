5ad927d5404b14a1546948aa3335ec2ff7817746
Maciej Sawicki <saw.maciej@gmail.com>
2017-04-08 01:02:35 +0200

    commit

--- main-code/v1/features/blending-colors.js
+++ main-code/v1/features/blending-colors.js
@@ -2,11 +2,12 @@ ReactiveLocalStorage.setDefaultParam('blendingColors', 'false');
 
 ReactiveLocalStorage.onParamChange('blendingColors', function(value) {
 
-	var currentTempo = ReactiveLocalStorage.getParam('tempo');
+	var tempo = ReactiveLocalStorage.getParam('tempo');
+	var tempoInMilliseconds = 60000/tempo;
 
 	if (value == 'true') {
 		$('.main-visualisations__fullscreen-color').addClass('is-blended')
-		.css('transition', 'background-color '+400+'ms');
+		.css('transition', 'background-color '+tempoInMilliseconds+'ms');
 	} else {
 		$('.main-visualisations__fullscreen-color').removeClass('is-blended');
 	}

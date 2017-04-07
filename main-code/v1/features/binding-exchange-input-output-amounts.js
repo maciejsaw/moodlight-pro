f4f8e152444ad81e89b0e4615382240ec7dacd7d
Maciej Sawicki <saw.maciej@gmail.com>
2017-04-07 20:54:13 +0200

    commit

--- main-code/v1/features/binding-exchange-input-output-amounts.js
+++
@@ -1,6 +0,0 @@
-ReactiveLocalStorageDataBindText({
-	"via-transfer__input-amount": 'via-transfer__input-amount',
-	"via-transfer__input-currency": 'via-transfer__input-currency',
-	"via-transfer__output-amount": 'via-transfer__output-amount',
-	"via-transfer__output-currency": 'via-transfer__output-currency',
-});

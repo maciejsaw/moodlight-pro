3d19177f308deeb89dedeca3a2a1c9bd433ad0ba
Maciej Sawicki <saw.maciej@gmail.com>
2017-04-09 09:56:51 +0200

    commit

---
+++ main-code/v1/features/presets-playing-and-saving.js
@@ -0,0 +1,12 @@
+ReactiveLocalStorageDataBindArrayList('listOfPresets', function($elementToAppend, elementData) {
+	//callback what to do with each item
+	//so you can modify each of then depending on state,
+
+	console.log(elementData);
+	$elementToAppend.find('[js-selector="preset-name"]').val(elementData.name);
+});
+
+$(document).on('click', '[action-play-preset]', function() {
+	var presetId = $(this).closest('[id]').attr('id');
+	console.log('presetId');
+});

99b85ebb18cc173fb63817c1871115860d54eb4a
Maciej Sawicki <saw.maciej@gmail.com>
2017-04-09 11:02:21 +0200

    commit

--- main-code/v1/features/presets-playing-and-saving.js
+++ main-code/v1/features/presets-playing-and-saving.js
@@ -1,16 +1,3 @@
-ReactiveLocalStorageDataBindArrayList('listOfPresets', function($elementToAppend, elementData) {
-	//callback what to do with each item
-	//so you can modify each of then depending on state,
-
-	console.log(elementData);
-	$elementToAppend.find('[js-selector="preset-name"]').val(elementData.name);
-});
-
-$(document).on('click', '[action-play-preset]', function() {
-	var presetId = $(this).closest('[id]').attr('id');
-	console.log('presetId');
-});
-
 ReactiveLocalStorage.setDefaultParam('listOfPresets', [
 	{
 		id: 'default1',
@@ -22,3 +9,17 @@ ReactiveLocalStorage.setDefaultParam('listOfPresets', [
 		color__4: 'null',
 	}
 ]);
+
+ReactiveLocalStorageDataBindArrayList('listOfPresets', function($elementToAppend, elementData) {
+	//callback what to do with each item
+	//so you can modify each of then depending on state,
+
+	console.log(elementData);
+	$elementToAppend.find('[js-selector="preset-name"]').val(elementData.name);
+});
+
+$(document).on('click', '[action-play-preset]', function() {
+	var presetId = $(this).closest('[id]').attr('id');
+	console.log(presetId);
+});
+

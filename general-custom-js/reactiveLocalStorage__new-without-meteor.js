0865313aef5aeca52815ae517ddd6a148aa5d4e4
Maciej Sawicki <saw.maciej@gmail.com>
2017-04-10 18:26:01 +0200

    commit

--- general-custom-js/reactiveLocalStorage__new-without-meteor.js
+++ general-custom-js/reactiveLocalStorage__new-without-meteor.js
@@ -1,21 +1,19 @@
 var ReactiveLocalStorage = (function() {
 
-	var paramsObject; //this will be be an object containing all params
 	var paramsString; //this will be a string containing params to save in local storage
 
 	//at the beginning, check if params are stored in local storage
 	if (typeof localStorage.getItem('paramsString') == 'undefined' || localStorage.getItem('paramsString') == null) {
-		paramsObject = {};
-		saveParamObjectToLocalStorageAsString();
+		var paramsObject = {};
+		saveParamObjectToLocalStorageAsString(paramsObject);
 	} else {
 		paramsString = localStorage.getItem('paramsString');
-		paramsObject = $.deparam('paramsString');
 	}
 
 	//each time this function is fired, the onParamChange (Meteor.run) functions will run, but only
 	//the ones for which the value changed
 	//this is used to 'transform' the static string into a reactive object after each update of the string param
-	function saveParamObjectToLocalStorageAsString() {
+	function saveParamObjectToLocalStorageAsString(paramsObject) {
 		paramsString = $.param(paramsObject);
 		localStorage.setItem('paramsString', paramsString);
 	}
@@ -23,15 +21,18 @@ var ReactiveLocalStorage = (function() {
 	function getParam(key) {
 		//this return only values, not direct access to paramsObject
 		//that's why we deparam here
-		return $.deparam(paramsString)[key]; 
+		return deparam(paramsString)[key]; 
 	}
 
 	function getAllParams() {
-		return $.deparam(paramsString);
+		return deparam(paramsString);
 	}
 
 	function setParam(key, value, options) {
 		options = options || {};
+
+		var paramsObject = deparam(paramsString);
+
 		console.log(value);
 		console.log(key);
 		console.log(paramsObject[key]);
@@ -39,14 +40,16 @@ var ReactiveLocalStorage = (function() {
 
 		if (paramsObject[key] !== value) {
 			paramsObject[key] = value;
-			saveParamObjectToLocalStorageAsString();
+			saveParamObjectToLocalStorageAsString(paramsObject);
 			console.log('reactiveLocalStorage__'+key+'__paramChanged__'+value+'');
-			$(document).trigger('reactiveLocalStorage__'+key+'__paramChanged', value); 
+			$(document).trigger('reactiveLocalStorage__'+key+'__paramChanged'); 
 		}
-		 
+
 	}
 
 	function setDefaultParam(key, value) {
+		var paramsObject = deparam(paramsString);
+
 		if (typeof paramsObject[key] == 'undefined') {
 			setParam(key, value); 
 		}
@@ -132,25 +135,29 @@ var ReactiveLocalStorage = (function() {
 	}
 
 	function removeParam(key, options) {
+		var paramsObject = deparam(paramsString);
+
 		options = options || {};
 
 		if (typeof paramsObject[key] !== 'undefined') {
 			delete paramsObject[key];
-			saveParamObjectToLocalStorageAsString();
-			$(document).trigger('reactiveLocalStorage__'+key+'__paramChanged', value); 
+			saveParamObjectToLocalStorageAsString(paramsObject);
+			$(document).trigger('reactiveLocalStorage__'+key+'__paramChanged'); 
 		}
 	}
 
 	function setFreshParams(newParamsObj) {
-		paramsObject = newParamsObj;
-		saveParamObjectToLocalStorageAsString();
+		var paramsObject = deparam(paramsString);
+		saveParamObjectToLocalStorageAsString(paramsObject);
 		retriggerOnParamChangeForAll();
 	}
 
 	var actionsOnParamChange = {};
 	function onParamChange(key, actionFunction) {
-		$(document).on('reactiveLocalStorage__'+key+'__paramChanged', function(event, value) {
-			console.log("action function for "+value);
+		$(document).on('reactiveLocalStorage__'+key+'__paramChanged', function(event) {
+			var paramsObject = deparam(paramsString);
+			var value = paramsObject[key];
+			console.log("action function for "+key);
 			actionFunction(value);
 			console.log(key+" on param change");
 		});
@@ -167,6 +174,7 @@ var ReactiveLocalStorage = (function() {
 	}
 
 	function retriggerOnParamChange(key) {
+		var paramsObject = deparam(paramsString);
 		var param = paramsObject[key];
 		console.log(param);
 		var arrayOfFunctionsAssociatedWithThisParam = actionsOnParamChange[key];

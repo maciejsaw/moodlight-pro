bc205ca8a36c92d08e492d7bdf147ea8b04cf3bf
Maciej Sawicki <saw.maciej@gmail.com>
2017-04-22 15:16:06 +0200

    no message

---
+++ main-code/v1/features/choosing-colors-from-palette.js
@@ -0,0 +1,22 @@
+var colorsPaletteTable = [
+	'yellow',
+	'pink',
+	'aqua'
+];
+
+function appendColorToDropdowns(colorName) {
+	var $dropdownChoiceTemplate = $('[color-choice-template]');
+	var $dropdownChosenTemplate = $('[color-chosen-template]');
+	var $dropdownChoicesToAppendTo = $('.colors-dropdown').find('.bem-select-dropdown__list.for-color-picker');
+	var $dropdownChosenItemsToAppendTo = $('.colors-dropdown').find('.bem-select-dropdown__button.for-color-icon');
+
+	var $newDropdownChoice = $dropdownChoiceTemplate.clone().find('.color-symbol-icon').css('background-color', colorName);
+	var $newDropdownChosen = $dropdownChosenTemplate.clone().find('.color-symbol-icon').css('background-color', colorName);
+
+	$dropdownChosenItemsToAppendTo.append($newDropdownChosen);
+	$dropdownChoicesToAppendTo.append($newDropdownChoice);
+}
+
+$.each(colorsPaletteTable, function(arrayIndex, arrayValue) {
+	//
+});

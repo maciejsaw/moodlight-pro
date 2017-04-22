a6f966fc1882cab98ee14325d8aa3e1101854ed6
Maciej Sawicki <saw.maciej@gmail.com>
2017-04-22 15:31:11 +0200

    auto commit

--- scripts.js
+++ scripts.js
@@ -2037,20 +2037,20 @@ var colorsPaletteTable = [
 ];
 
 function appendColorToDropdowns(colorName) {
-	var $dropdownChoiceTemplate = $('[color-choice-template]');
-	var $dropdownChosenTemplate = $('[color-chosen-template]');
+	var $dropdownChoiceTemplate = $('[color-choice-template]').first();
+	var $dropdownChosenTemplate = $('[color-chosen-template]').first();
 	var $dropdownChoicesToAppendTo = $('.colors-dropdown').find('.bem-select-dropdown__list.for-color-picker');
 	var $dropdownChosenItemsToAppendTo = $('.colors-dropdown').find('.bem-select-dropdown__button.for-color-icon');
 
 	var $newDropdownChoice = $dropdownChoiceTemplate.clone().find('.color-symbol-icon').css('background-color', colorName);
-	var $newDropdownChosen = $dropdownChosenTemplate.clone().find('.color-symbol-icon').css('background-color', colorName);
+	var $newDropdownChosen = $dropdownChosenTemplate.clone().find('.color-symbol-icon').css('background-color', colorName).addClass('is-hidden');
 
 	$dropdownChosenItemsToAppendTo.append($newDropdownChosen);
 	$dropdownChoicesToAppendTo.append($newDropdownChoice);
 }
 
 $.each(colorsPaletteTable, function(arrayIndex, arrayValue) {
-	//
+	//////
 });
 
 //# sourceMappingURL=scripts.js.map

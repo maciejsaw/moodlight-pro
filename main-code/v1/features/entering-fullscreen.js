bc5fd39e3a10f674bf78f57afa95d072d4002d39
Maciej Sawicki <saw.maciej@gmail.com>
2017-04-22 17:12:33 +0200

    auto commit

--- main-code/v1/features/entering-fullscreen.js
+++ main-code/v1/features/entering-fullscreen.js
@@ -1,7 +1,7 @@
 //this will automatically go into fullscreen mode whenever play is triggered
 ReactiveLocalStorage.onParamChange('startedOrStopped', function(value) {
 	if (value === 'started') {
-		$('body').fullscreen({overflow: 'visible'});
+		$('.body-wrapper').fullscreen();
 		setTimeout(function() {
 			enterFullscreenMode(); //also hides the menu
 		}, 1000);

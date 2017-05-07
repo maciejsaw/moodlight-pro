1cb82fe8dc6cf0b86ebf6f9c8bce7ff5f1ef2346
Maciej Sawicki <saw.maciej@gmail.com>
2017-05-07 23:45:34 +0200

    auto commit

--- main-code/v1/features/entering-fullscreen.js
+++ main-code/v1/features/entering-fullscreen.js
@@ -1,10 +1,11 @@
 //this will automatically go into fullscreen mode whenever play is triggered
-ReactiveLocalStorage.onParamChange('startedOrStopped', function(value) {
-	if (value === 'started') {
-		$('.body-wrapper').fullscreen();
-		setTimeout(function() {
-			enterFullscreenMode(); //also hides the menu
-		}, 1000);
-	}
-});		
-
+if (typeof window.cordova === 'undefined') {
+	ReactiveLocalStorage.onParamChange('startedOrStopped', function(value) {
+		if (value === 'started') {
+			$('.body-wrapper').fullscreen();
+			setTimeout(function() {
+				enterFullscreenMode(); //also hides the menu
+			}, 1000);
+		}
+	});		
+}
--- scripts.js
+++ scripts.js
@@ -2852,16 +2852,16 @@ ReactiveLocalStorage.retriggerOnParamChange('color__3');
 ReactiveLocalStorage.retriggerOnParamChange('color__4');
 
 //this will automatically go into fullscreen mode whenever play is triggered
-ReactiveLocalStorage.onParamChange('startedOrStopped', function(value) {
-	if (value === 'started') {
-		$('.body-wrapper').fullscreen();
-		setTimeout(function() {
-			enterFullscreenMode(); //also hides the menu
-		}, 1000);
-	}
-});		
-
-
+if (typeof window.cordova === 'undefined') {
+	ReactiveLocalStorage.onParamChange('startedOrStopped', function(value) {
+		if (value === 'started') {
+			$('.body-wrapper').fullscreen();
+			setTimeout(function() {
+				enterFullscreenMode(); //also hides the menu
+			}, 1000);
+		}
+	});		
+}
 
 $(document).on('click', '[action-play-preset-1]', function() {
 	triggerPlayOfPresetOfIndexX(0);
